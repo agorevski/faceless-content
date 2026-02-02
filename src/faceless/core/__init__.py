5f87305be4460b64eb0595028df2496cb2b90c99
Alex Gorevski <alex.gorevski@live.com>
2026-02-01 19:24:48 -0800

    Update module exports for new core and service modules
    Update __init__.py files to export all new functionality:
    
    core/__init__.py:
    - Export hooks (FIRST_FRAME_HOOKS, PATTERN_INTERRUPTS, etc.)
    - Export hashtags (HASHTAG_LADDER, generate_hashtag_set, etc.)
    - Export tiktok_formats (TikTokFormat, get_format, etc.)
    - Export posting_schedule (POSTING_WINDOWS, get_optimal_posting_time)
    - Export text_overlay (TextOverlay, TextStyle, overlay_to_ffmpeg_filter)
    
    services/__init__.py:
    - Export metadata_service functions
    - Export scraper_service functions
    - Export subtitle_service functions and SUBTITLE_STYLES
    - Export thumbnail_service functions and THUMBNAIL_TEMPLATES

--- src/faceless/core/__init__.py
+++ src/faceless/core/__init__.py
@@ -5,6 +5,11 @@ This module contains the fundamental building blocks:
 - Enums: Niche, Platform, JobStatus
 - Models: Scene, Script, Job, VisualStyle
 - Exceptions: Custom exception hierarchy
+- Hooks: TikTok engagement hooks and retention strategies
+- Hashtags: Hashtag ladder system for content discovery
+- TikTok Formats: Content format definitions
+- Posting Schedule: Optimal posting time strategies
+- Text Overlay: Text overlay models for video
 """
 
 from faceless.core.enums import JobStatus, Niche, Platform
@@ -18,7 +23,69 @@ from faceless.core.exceptions import (
     ValidationError,
     VideoAssemblyError,
 )
+from faceless.core.hashtags import (
+    HASHTAG_LADDER,
+    TRENDING_TOPICS,
+    analyze_hashtag_coverage,
+    generate_hashtag_set,
+    generate_hashtag_string,
+    get_all_hashtags,
+    get_format_specific_hashtags,
+    get_series_suggestions,
+)
+from faceless.core.hooks import (
+    COMMENT_TRIGGERS,
+    FIRST_FRAME_HOOKS,
+    LOOP_STRUCTURES,
+    MID_VIDEO_HOOKS,
+    PATTERN_INTERRUPTS,
+    PINNED_COMMENTS,
+    generate_engagement_package,
+    get_comment_trigger,
+    get_first_frame_hook,
+    get_loop_structure,
+    get_mid_video_hook,
+    get_pattern_interrupt,
+    get_pinned_comment,
+)
 from faceless.core.models import Job, Scene, Script, VisualStyle
+from faceless.core.posting_schedule import (
+    DAY_PATTERNS,
+    FREQUENCY_RECOMMENDATIONS,
+    POSTING_WINDOWS,
+    format_schedule_for_display,
+    generate_weekly_schedule,
+    get_day_rating,
+    get_next_optimal_slot,
+    get_optimal_posting_time,
+)
+from faceless.core.text_overlay import (
+    PRESET_STYLES,
+    TextAnimation,
+    TextOverlay,
+    TextPosition,
+    TextStyle,
+    create_countdown_overlays,
+    create_cta_overlay,
+    create_hook_overlay,
+    create_mid_video_overlay,
+    create_pov_overlay,
+    generate_overlay_filter_chain,
+    overlay_to_ffmpeg_filter,
+    position_to_xy,
+)
+from faceless.core.tiktok_formats import (
+    ALL_FORMATS,
+    FINANCE_FORMATS,
+    LUXURY_FORMATS,
+    SCARY_FORMATS,
+    TikTokFormat,
+    format_to_prompt_guidance,
+    get_all_formats_for_niche,
+    get_format,
+    get_format_names,
+    get_random_format,
+)
 
 __all__ = [
     # Enums
@@ -39,4 +106,61 @@ __all__ = [
     "ImageGenerationError",
     "TTSGenerationError",
     "VideoAssemblyError",
+    # Hooks
+    "FIRST_FRAME_HOOKS",
+    "PATTERN_INTERRUPTS",
+    "MID_VIDEO_HOOKS",
+    "COMMENT_TRIGGERS",
+    "PINNED_COMMENTS",
+    "LOOP_STRUCTURES",
+    "get_first_frame_hook",
+    "get_pattern_interrupt",
+    "get_mid_video_hook",
+    "get_comment_trigger",
+    "get_pinned_comment",
+    "get_loop_structure",
+    "generate_engagement_package",
+    # Hashtags
+    "HASHTAG_LADDER",
+    "TRENDING_TOPICS",
+    "generate_hashtag_set",
+    "generate_hashtag_string",
+    "get_all_hashtags",
+    "get_format_specific_hashtags",
+    "get_series_suggestions",
+    "analyze_hashtag_coverage",
+    # TikTok Formats
+    "TikTokFormat",
+    "SCARY_FORMATS",
+    "FINANCE_FORMATS",
+    "LUXURY_FORMATS",
+    "ALL_FORMATS",
+    "get_format",
+    "get_all_formats_for_niche",
+    "get_format_names",
+    "get_random_format",
+    "format_to_prompt_guidance",
+    # Posting Schedule
+    "POSTING_WINDOWS",
+    "DAY_PATTERNS",
+    "FREQUENCY_RECOMMENDATIONS",
+    "get_optimal_posting_time",
+    "get_day_rating",
+    "generate_weekly_schedule",
+    "get_next_optimal_slot",
+    "format_schedule_for_display",
+    # Text Overlay
+    "TextPosition",
+    "TextAnimation",
+    "TextStyle",
+    "TextOverlay",
+    "PRESET_STYLES",
+    "create_hook_overlay",
+    "create_mid_video_overlay",
+    "create_cta_overlay",
+    "create_countdown_overlays",
+    "create_pov_overlay",
+    "position_to_xy",
+    "overlay_to_ffmpeg_filter",
+    "generate_overlay_filter_chain",
 ]
--- src/faceless/services/__init__.py
+++ src/faceless/services/__init__.py
@@ -9,17 +9,54 @@ This module provides the business logic services for content generation:
 - DeepResearchService: Conduct deep research on topics
 - QualityService: Evaluate script quality and hooks
 - TrendingService: Discover trending topics
+- SubtitleService: Generate subtitles (SRT/VTT)
+- ThumbnailService: Generate thumbnails
+- ScraperService: Fetch content from sources
+- MetadataService: Generate posting metadata
 """
 
 from faceless.services.enhancer_service import EnhancerService
 from faceless.services.image_service import ImageService
+from faceless.services.metadata_service import (
+    format_metadata_for_display,
+    generate_content_metadata,
+    generate_series_metadata,
+    load_metadata,
+    save_metadata,
+)
 from faceless.services.quality_service import QualityService
 from faceless.services.research_service import DeepResearchService, ResearchDepth
+from faceless.services.scraper_service import (
+    clean_text,
+    fetch_and_process_stories,
+    fetch_creepypasta,
+    fetch_reddit_stories,
+    generate_image_prompt,
+    save_story,
+    story_to_script,
+)
+from faceless.services.subtitle_service import (
+    SUBTITLE_STYLES,
+    burn_subtitles_to_video,
+    create_subtitles_from_audio,
+    create_subtitles_from_script,
+    generate_all_subtitle_formats,
+    generate_animated_captions,
+)
+from faceless.services.thumbnail_service import (
+    THUMBNAIL_CONCEPTS,
+    THUMBNAIL_TEMPLATES,
+    create_text_overlay_instructions,
+    generate_thumbnail,
+    generate_thumbnail_prompt,
+    generate_thumbnail_variants,
+)
 from faceless.services.trending_service import TrendingService
 from faceless.services.tts_service import TTSService
 from faceless.services.video_service import VideoService
 
 __all__ = [
+    # Main Services
     "EnhancerService",
     "ImageService",
     "TTSService",
@@ -28,4 +65,32 @@ __all__ = [
     "ResearchDepth",
     "QualityService",
     "TrendingService",
+    # Subtitle Service
+    "SUBTITLE_STYLES",
+    "create_subtitles_from_script",
+    "create_subtitles_from_audio",
+    "burn_subtitles_to_video",
+    "generate_animated_captions",
+    "generate_all_subtitle_formats",
+    # Thumbnail Service
+    "THUMBNAIL_TEMPLATES",
+    "THUMBNAIL_CONCEPTS",
+    "generate_thumbnail_prompt",
+    "generate_thumbnail",
+    "generate_thumbnail_variants",
+    "create_text_overlay_instructions",
+    # Scraper Service
+    "clean_text",
+    "fetch_reddit_stories",
+    "fetch_creepypasta",
+    "save_story",
+    "story_to_script",
+    "generate_image_prompt",
+    "fetch_and_process_stories",
+    # Metadata Service
+    "generate_content_metadata",
+    "generate_series_metadata",
+    "save_metadata",
+    "load_metadata",
+    "format_metadata_for_display",
 ]
